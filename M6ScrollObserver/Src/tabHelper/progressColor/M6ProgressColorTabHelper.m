ae6f69c67ab294e918f547c5f8f9393e8a75aa16
chenmeisong <chenmeisong@baidu.com>
2019-01-25 22:03:25 +0800

    u

--- M6ScrollObserver/Src/tabHelper/progressColor/M6ProgressColorTabHelper.m
+++ M6ScrollObserver/Src/tabHelper/progressColor/M6ProgressColorTabHelper.m
@@ -35,19 +35,20 @@
     CGFloat deltaR = [self.selectedColorRGB[0] floatValue] - [self.normalColorRGB[0] floatValue];
     CGFloat deltaG = [self.selectedColorRGB[1] floatValue] - [self.normalColorRGB[1] floatValue];
     CGFloat deltaB = [self.selectedColorRGB[2] floatValue] - [self.normalColorRGB[2] floatValue];
-    self.deltaRGB = [NSArray arrayWithObjects:@(deltaR), @(deltaG), @(deltaB), nil];
+    CGFloat deltaA = [self.selectedColorRGB[3] floatValue] - [self.normalColorRGB[3] floatValue];
+    self.deltaRGB = [NSArray arrayWithObjects:@(deltaR), @(deltaG), @(deltaB), @(deltaA), nil];
 }
 
 - (M6CurrentColors *)currentColorsWithProgress:(CGFloat)progress {
     UIColor *fromColor = [UIColor colorWithRed:[self.normalColorRGB[0] floatValue] + [self.deltaRGB[0] floatValue] * (1 - progress)
                                          green:[self.normalColorRGB[1] floatValue] + [self.deltaRGB[1] floatValue] * (1 - progress)
                                           blue:[self.normalColorRGB[2] floatValue] + [self.deltaRGB[2] floatValue] * (1 - progress)
-                                         alpha:1];
+                                         alpha:[self.normalColorRGB[3] floatValue] + [self.deltaRGB[3] floatValue] * (1 - progress)];
     
     UIColor *toColor = [UIColor colorWithRed:[self.normalColorRGB[0] floatValue] + [self.deltaRGB[0] floatValue] * progress
                                        green:[self.normalColorRGB[1] floatValue] + [self.deltaRGB[1] floatValue] * progress
                                         blue:[self.normalColorRGB[2] floatValue] + [self.deltaRGB[2] floatValue] * progress
-                                       alpha:1];
+                                       alpha:[self.normalColorRGB[3] floatValue] + [self.deltaRGB[3] floatValue] * progress];
     
     M6CurrentColors *currentColors = [M6CurrentColors new];
     currentColors.currentFromColor = fromColor;
@@ -62,9 +63,9 @@
     NSArray *colorRGB = nil;
     const CGFloat *components = CGColorGetComponents(color.CGColor);
     if (count == 4) {
-        colorRGB = [NSArray arrayWithObjects:@(components[0]), @(components[1]), @(components[2]), nil];
+        colorRGB = [NSArray arrayWithObjects:@(components[0]), @(components[1]), @(components[2]), @(components[3]), nil];
     } else if (count == 2) {
-        colorRGB = [NSArray arrayWithObjects:@(components[0]), @(components[0]), @(components[0]), nil];
+        colorRGB = [NSArray arrayWithObjects:@(components[0]), @(components[0]), @(components[0]), @(components[1]), nil];
     } else {}
     
     return colorRGB;
